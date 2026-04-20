af089ee2c423e27b984b30a899e2bff3d2f3c9ee
Premveer Singh <premveersinghyadav207@gmail.com>
2026-04-20 14:47:01 +0530

    fix os error

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
+from flask import Flask
 import os
-
-from flask import Flask, request, jsonify
-from db import get_conn
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Milk Managment API running"
+    return "Milk Management API Running"
 
 @app.route("/test")
 def test():
     return {"message": "API working"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
-    port=int(os.environ.get("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
