396f2dbe92f1adcde9e45e8794a50c5a44ff69ec
Premveer Singh <premveersinghyadav207@gmail.com>
2026-04-20 13:38:03 +0530

    Day 8 deploy setup

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def get_customers():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)))power
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 1000)))
